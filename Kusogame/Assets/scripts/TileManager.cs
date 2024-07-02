20900badb08f4f72b11055bcf4b0d99cbd7c536f
HelpHelpHelp-Charlie <59757392+HelpHelpHelp-Charlie@users.noreply.github.com>
2024-07-02 21:50:47 +0800

    add Example3d project

--- Kusogame/Assets/scripts/TileManager.cs
+++ Kusogame/Assets/scripts/TileManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,37 @@ using UnityEngine;
 public class TileManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject[] TiltPrefabs;
+    public float zSpawn=0;
+    public float tiltLength=5;
+    public Transform playerTransform;
+    public int total_game_tile= 10;
+
+    public int sightDistance=2;
+
     void Start()
     {
-        
+        initspawnTile();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        if(playerTransform.position.z>zSpawn-sightDistance*tiltLength && zSpawn<=tiltLength*total_game_tile+7){
+            spawnTile(UnityEngine.Random.Range(2,TiltPrefabs.Length));
+            Debug.Log("Hello");
+        }
+
+    }
+    public void spawnTile(int index){
+        Instantiate(TiltPrefabs[index],transform.forward*zSpawn,transform.rotation);
+        zSpawn+=tiltLength; 
+    }
+    public void initspawnTile(){
+        Instantiate(TiltPrefabs[0],transform.forward*zSpawn,transform.rotation);
+        zSpawn+=7F; 
     }
 }
---
+++ Kusogame/Assets/scripts/adder.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class adder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int add_number=0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
